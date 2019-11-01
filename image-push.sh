3ce7c7e7318793b2b73306a2c81d1f19043ae596
Mehtaab Brar <mehtaab.brar@accenture.com>
2019-10-31 19:43:37 -0700

    Add parameter to image-push script

--- image-push.sh
+++ image-push.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+if [[ ! -v $1 ]]
+then
+	echo 'Specify an ECR name'
+	exit
+fi
+
 sudo service docker start
-docker build --no-cache -t wb-app/docker-images .
+docker build --no-cache -t $1 .
 $(aws ecr get-login --no-include-email)
-ecr_repo_url=$(aws ecr describe-repositories --repository-names wb-app/docker-images --query "repositories[0].repositoryUri")
+ecr_repo_url=$(aws ecr describe-repositories --repository-names $1 --query "repositories[0].repositoryUri")
 echo $ecr_repo_url
 docker tag wb-app/docker-images:latest "$ecr_repo_url:latest"
 docker push "$ecr_repo_url:latest"
